6f06e843df9f170aa104ff7f10f1a1edafda19c1
Anandha Padmanaban Prasad <padmanaban.prasad@utoronto.ca>
2018-11-11 13:29:43 -0500

    Modifed the install script and added a sh snippets file

--- installVundle.sh
+++ installVundle.sh
@@ -6,59 +6,61 @@ bundle=~/.vim/bundle
 # Check if the bundle folder exists, create if it does not
 if [ ! -d "$bundle" ];
 then
-        mkdir "$bundle"
+    mkdir "$bundle"
 fi
 
 #------------------------------------------------------------------------------
 echo Cloning the Vundle bundle from github to the bundle directory...
 #------------------------------------------------------------------------------
 
-# Clone the Vundle bundle from gitbub to the bundle directory
-git clone https://github.com/VundleVim/Vundle.vim.git "$bundle"/Vundle.vim 2>/d\
-ev/null
+# Clone the Vundle bundle from gitbub to the bundle directory, delete if it
+# already exists
+[ -d "$bundle"/Vundle.vim ] && rm -Rf "$bundle"/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git "$bundle"/Vundle.vim \
+2>/dev/null
 
 # If cloning failed then simply download the snapshot of the bundle
 if [ "$?" != "0" ];
 then
-        #----------------------------------------------------------------------
-        echo Cloning Filed. Downloading the Snapshot...
-        #----------------------------------------------------------------------
-        wget -qO "$bundle"/master.zip https://github.com/VundleVim/Vundle.vim/\
+    #--------------------------------------------------------------------------
+    echo Cloning Failed. Downloading the Snapshot...
+    #--------------------------------------------------------------------------
+    wget -qO "$bundle"/master.zip https://github.com/VundleVim/Vundle.vim/\
 archive/master.zip 2>/dev/null
-        unzip "$bundle"/master.zip -d "$bundle" 2>/dev/null
-        rm -f "$bundle"/master.zip
-        mv "$bundle"/Vundle* "$bundle"/Vundle.vim
-        #----------------------------------------------------------------------
-        echo Downloaded the Snapshot of Vundle
-        #----------------------------------------------------------------------
+    unzip "$bundle"/master.zip -d "$bundle" 2>/dev/null
+    rm -f "$bundle"/master.zip
+    mv "$bundle"/Vundle* "$bundle"/Vundle.vim/
+    #--------------------------------------------------------------------------
+    echo Downloaded the Snapshot of Vundle
+    #--------------------------------------------------------------------------
 else
-        #----------------------------------------------------------------------
-        echo Cloning complete
-        #----------------------------------------------------------------------
+    #--------------------------------------------------------------------------
+    echo Cloning complete
+    #--------------------------------------------------------------------------
 fi
 
-# Create a symbolic line to the vimrc from the home direcory
+# Create a symbolic link to the vimrc from the home direcory
 ln -s ~/.vim/.vimrc ~/.vimrc 2>/dev/null
 
 # Check if the link succeeded, if not, simply ask the user to make a link manu
 # -ally
-if [ "$?" != "0" ];
+if [ $? -ne 0 ];
 then
-        #----------------------------------------------------------------------
-        echo Creating a symbolic link for the .vimrc failed since there alread\
-                y exits one. Please copy the .vimrc to your home accordingly
-        #----------------------------------------------------------------------
-        exit 1
+    # Find the the nth backup to rename the existing conf file to
+    backcounter=0
+    while [ -f ~/.vimrc.bak$(expr $backcounter + 1) ]
+    do
+        backcounter=$(expr $backcounter + 1)
+    done
+    backcounter=$(expr $backcounter + 1)
+
+    # Create a backup of the already present vimrc and notify the user
+    mv ~/.vimrc ~/.vimrc.bak$backcounter
+    echo Backed up ~/.vimrc to ~/.vimrc.bak$backcounter
+
+    ln -s ~/.vim/.vimrc ~/.vimrc
 fi
 
-# Install the vundle plugin
-if ! vim -c "call InstallVundle()" ;
-then
-        #----------------------------------------------------------------------
-        echo Installation failed.
-        #----------------------------------------------------------------------
-else
-        #----------------------------------------------------------------------
-        echo Installation successful
-        #----------------------------------------------------------------------
-fi
+# Install all the plugins specified in the conf
+vim +PluginInstall +qall
+
